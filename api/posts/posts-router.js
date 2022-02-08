d9d9fd74e8307dacf896bc5cedc9c0a54fc41a94
joeys1122 <josephs1122@gmail.com>
2022-02-08 15:44:08 -0500

    added post and put endpoints

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -28,4 +28,46 @@ router.get('/:id', (req, res) => {
     })
 });
 
+router.post('/', (req, res) => {
+  if(!req.body.title || !req.body.contents) {
+    res.status(400).json({ message: "Please provide title and contents for the post" });
+  } else {
+    Post.insert(req.body)
+      .then(post => {
+        return Post.findById(post.id)
+      })
+      .then(post => {
+        res.status(201).json(post);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: "There was an error while saving the post to the database" });
+      })
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if(!post) {
+      res.status(404).json({ message: "The post with the specified ID does not exist" });
+      return;
+    }
+
+    if(!req.body.title || !req.body.contents) {
+      res.status(400).json({ message: "Please provide title and contents for the post" });
+      return;
+    } else {
+      const updatePost = await Post.update(req.params.id, req.body);
+      if(updatePost) {
+        const newPost = await Post.findById(req.params.id);
+        res.status(200).json(newPost)
+      }
+    }
+  } catch(err) {
+    console.log(err);
+    res.status(500).json({ message: "The post information could not be modified" });
+  } 
+});
+
 module.exports = router;
