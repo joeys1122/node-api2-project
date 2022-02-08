a372edfccf9962e627c50bc7b299baed5b49360d
joeys1122 <josephs1122@gmail.com>
2022-02-08 13:53:15 -0500

    setup posts router and GET enpoints

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,31 @@
 // implement your posts router here
+const { Router } = require('express');
+const Post = require('./posts-model');
+
+const router = Router();
+
+router.get('/', (req, res) => {
+  Post.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "The posts information could not be retrieved" });
+    })
+});
+
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      post ? 
+      res.status(200).json(post) :
+      res.status(404).json({ message: "The post with the specified ID does not exist" })
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "The post information could not be retrieved" })
+    })
+});
+
+module.exports = router;
