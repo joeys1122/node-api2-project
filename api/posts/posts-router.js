1474cc844f3146eda75e19cfb21681f1e7eef711
joeys1122 <josephs1122@gmail.com>
2022-02-08 16:01:28 -0500

    added delete and get comments endpoints

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -70,4 +70,34 @@ router.put('/:id', async (req, res) => {
   } 
 });
 
+router.delete('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if(!post) {
+      res.status(404).json({ message: "The post with the specified ID does not exist" });
+    } else {
+      await Post.remove(req.params.id)
+      res.json(post);
+    }
+  } catch(err) {
+    console.log(err)
+    res.status(500).json({ message: "The post could not be removed" });
+  }
+});
+
+router.get('/:id/comments', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if(!post) {
+      res.status(404).json({ message: "The post with the specified ID does not exist" });
+    } else {
+      const comments = await Post.findPostComments(req.params.id);
+      res.status(200).json(comments);
+    }
+  } catch(err) {
+    console.log(err);
+    res.status(500).json({ message: "The comments information could not be retrieved" });
+  }
+});
+
 module.exports = router;
